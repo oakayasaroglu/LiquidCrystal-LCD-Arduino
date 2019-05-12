34141efd4a4e5b59ed100dcdb034846b4705901c
aosmank <a.osmank@icloud.com>
2019-05-12 19:02:39 +0300

    Update sketch_may08a.ino

--- lcddht/sketch_may08a/sketch_may08a.ino
+++ lcddht/sketch_may08a/sketch_may08a.ino
@@ -2,13 +2,13 @@
 #include "DHT.h"
 
 
-#define DHTPIN 12
+#define DHTPIN 11
  
 LiquidCrystal lcd(8,7,6,5,4,3);
 
 
 #define DHTTYPE DHT11
-float t,h;
+float scklk,nm;
 
 DHT   dht(DHTPIN,DHTTYPE);
 
@@ -22,16 +22,17 @@ void setup() {
 void loop() {
   delay(2000);
   
-  t = dht.readTemperature();
-  h = dht.readHumidity();
+  scklk = dht.readTemperature();
+  nm = dht.readHumidity();
   
   lcd.clear();
   lcd.setCursor(0,0);
+//  Serial.write(t);
   lcd.print("Sicaklik:");
-  lcd.print(t);
+  lcd.print(scklk);
   lcd.print("\337C");
 
   lcd.setCursor(0,1);
   lcd.print("Nem:     %");
-  lcd.print(h);
+  lcd.print(nm);
 }
